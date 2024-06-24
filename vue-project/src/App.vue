b677be92164855711f3a7ec8d5cd839c83404100
wengangfei@gmail.com <wengangfei@gmail.com>
2024-06-24 16:52:27 -0400

    added

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,68 +1,78 @@
 <script>
-  import BaseCounter from './components/BaseCounter.vue';
-  import ListInfo from './components/ListInfo.vue';
+import BaseCounter from './components/BaseCounter.vue'
+import ListInfo from './components/ListInfo.vue'
 
-  export default {
-    //register a component in to components property.
-    components:{
-      BaseCounter,
-      ListInfo,
-    },
+export default {
+  //register a component in to components property.
+  components: {
+    BaseCounter,
+    ListInfo
+  },
 
-    //data function is to define the reactive state for a component
-    //when these prop are updated, vue will auto update the DOM
-    //to reflect the changes.
-    data() {
-      //return a configuration object.
-      return {
-        doctor:{
-          name:'pai',
-          age:30,
-          food:'hotPot',
-          location:'china',
-          color:'yellow,'
-        }
-
-      }
-    },
-
-    computed:{
-      getPersonInfo(){
-        //pass an object to children component
-        return {
-          name: this.doctor.name,
-          color: this.doctor.color,
-          age:this.doctor.age
-        }
-      }
-
-    },
-
-    methods:{
-      showInfo(){
-        return {
-          food:this.doctor.food,
-          location:this.doctor.location,
-        }
-      }
-    },
-
-    watch: {
-      //watch method is watching for all data changes.
-      // watching the number and auto => newValue
-      //to Avoid the heavy computation reactive data, move it to computed
-      //property to
-      number(newValue) {
-        newValue % 2 === 0 ? (this.counter = 'Even Counter') : (this.counter = 'Odd Counter')
+  //data function is to define the reactive state for a component
+  //when these prop are updated, vue will auto update the DOM
+  //to reflect the changes.
+  data() {
+    //return a configuration object.
+    return {
+      doctor: {
+        name: 'pai',
+        age: 30,
+        food: 'hotPot',
+        location: 'china',
+        color: 'yellow,'
       }
     }
+  },
+
+  computed: {
+    getPersonInfo() {
+      //pass an object to children component
+      return {
+        name: this.doctor.name,
+        color: this.doctor.color,
+        age: this.doctor.age
+      }
+    }
+  },
+
+  methods: {
+    showInfo() {
+      return {
+        food: this.doctor.food,
+        location: this.doctor.location
+      }
+    },
+
+    changeName() {
+      this.doctor.name = 'gun'
+    },
+    ageChange() {
+      this.doctor.age = 18
+    }
+  },
+
+  watch: {
+    //watch method is watching for all data changes.
+    // watching the number and auto => newValue
+    //to Avoid the heavy computation reactive data, move it to computed
+    //property to
+    number(newValue) {
+      newValue % 2 === 0 ? (this.counter = 'Even Counter') : (this.counter = 'Odd Counter')
+    }
   }
+}
 </script>
 
 <template>
   <h1>Vue practice</h1>
   <div>
-   <ListInfo :fun="getPersonInfo" :med="showInfo"/>
+    <ListInfo
+      :fun="getPersonInfo"
+      :med="showInfo"
+      @change-name="changeName"
+      @change-age="ageChange"
+    />
     <hr />
     <!-- Counter component injected -->
     <BaseCounter />
--- vue-project/src/components/ListInfo.vue
+++ vue-project/src/components/ListInfo.vue
@@ -1,45 +1,72 @@
 <script>
-    export default {
-        props:{
-            fun:{
-                type: Object,
-                required: true,
-            },
-            med:{
-                type: Function,
-                required: true,
-            }
-        },
-
-        setup () {
-            return {
-                message: 'Come on!',
-                listOfNumbers: [
-                { id: '1', name: 'fei', n: [11, 22, 33] },
-                { id: '2', name: 'wen', n: [11, 22, 33] },
-                { id: '3', name: 'gang', n: [11, 22, 33] }
-                ],
-            }
-        }
+import NameCard from './NameCard.vue'
+export default {
+  props: {
+    fun: {
+      type: Object,
+      required: true
+    },
+    med: {
+      type: Function,
+      required: true
     }
+  },
+
+  emits: ['change-name', 'change-age'],
+
+  methods: {
+    changeDocName() {
+      this.$emit('change-name')
+    },
+
+    changeAge() {
+      this.$emit('change-age')
+    },
+
+    addNumber(id) {
+      this.listOfNumbers.find((item) => item.id === id).n.push(this.numAdded)
+      //reset the input
+      this.val.target.value = ''
+    },
+    numToAdd(e) {
+      this.numAdded = Number(e.target.value)
+      this.val = e
+    }
+  },
+
+  data() {
+    return {
+      message: 'Come on!',
+      listOfNumbers: [
+        { id: '1', name: 'fei', n: [1, 2] },
+        { id: '2', name: 'wen', n: [4, 8, 3] },
+        { id: '3', name: 'gang', n: [5, 7, 9] }
+      ],
+      numAdded: 0,
+      val: ''
+    }
+  }
+  //registered the component
+  //   components: { NameCard }
+}
 </script>
 
-
 <template>
-    <div>
-        <h3>Name:{{ fun.name }}</h3>
-        <h3>Color:{{ fun.color }}</h3>
-        <h3>Age:{{ fun.age }}</h3>
-        <h3>Location:{{ med.location }}</h3>
-        <h3>Food:{{ med.food }}</h3>
-        <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
-        <p v-else>Odd: {{ message }}</p>
-        <div v-for="num in listOfNumbers" :key="num.id">
-        {{ num.name }}:
-        <span v-for="item in num.n" :key="item">{{ item }}</span>
-        </div>
+  <div>
+    <h3>Name:{{ fun.name }}</h3>
+    <button @click="changeDocName">Change Name</button>
+    <h3>Color:{{ fun.color }}</h3>
+    <h3>Age:{{ fun.age }}</h3>
+    <button @click="changeAge">Change Age</button>
+    <h3>Location:{{ med.location }}</h3>
+    <!-- <h3>Food:{{ console.log(med()) }}</h3> -->
+    <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
+    <p v-else>Odd: {{ message }}</p>
+    <div v-for="num in listOfNumbers" :key="num.id">
+      {{ num.name }}:
+      <span v-for="item in num.n" :key="item">{{ item }}{{ ', ' }}</span>
+      <input type="text" @input="numToAdd" />
+      <button @click="addNumber(num.id)">Add a Number</button>
     </div>
+  </div>
 </template>
-
-
-
