98e9f4623fe2997c1e06f3368c3215952bc1cb78
Fei <wengangfei@gmail.com>
2024-11-02 11:09:08 -0400

    added

---
+++ Vue_practice/client/src/components/ChildCom.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input class="border-2" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+        <input class="border-2" :value="nextName" @input="$emit('update:nextName', $event.target.value)" />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            modelValue: {
+                type: String,
+                required: true
+            },
+
+            nextName: {
+                type: String,
+                required: true
+            } 
+        },
+
+        // emits: ['update:modelValue']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue_practice/client/src/components/ParentCom.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ChildCom v-model:modelValue="name1" v-model:nextName="name2"/>
+        Name: {{ name1 }}
+        Name2: {{ name2 }}
+    </div>
+</template>
+
+<script>
+    import ChildCom from './ChildCom.vue';
+    export default {
+        data(){
+            return{
+                name1:'default',
+                name2:'default2',
+            }
+        },
+
+        components:{
+            ChildCom
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
