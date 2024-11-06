02137dd4b29051d9f929b3cea203c7ac10481b63
Fei <wengangfei@gmail.com>
2024-11-05 21:58:28 -0500

    added

---
+++ Vue_practice/client/src/components/FreshStart.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="bg-red-400 text-white px-1 rounded-md"
+    @click="($event) => count++"
+  >
+    Fresh start: {{ count }}
+  </button>
+  {{ name }}
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+const props = defineProps({
+  name:{
+    type: String,
+    default: "Fei"
+  }
+});
+
+const count = ref(0);
+</script>
